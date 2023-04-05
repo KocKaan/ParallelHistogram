f4da7c001924d826168dc6ce9e2802ee826a4fe0
kk4291 <kk4291@nyu.edu>
2023-04-04 23:56:24 -0400

    Last touch

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ParallelHistogram
+
+Assume your program is called histogram, the command line is expected to be:
+mpiexec -n x ./histogram b filename
+Where:
+b: is the number of bins, 0 < b <= 50
+x: number of processes, 0 < t <= 50
+filename: the name of the text file that contains the floating point numbers
+
+You compile your code with:
+mpicc -Wall -std=c99 -o histogram main.c
+
+Do the compilation and execution on crunchyx (x = 1, 3, 5, or 6) machines.
--- main.c
+++ main.c
@@ -7,15 +7,15 @@ int main(int argc, char *argv[]) {
   int rank, size;
   int num_elements, num_bins;
   double range_size, num;
-  //int *data;
   int *local_histogram, *global_histogram;
   char *filename;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  printf("The size is %d \n",size);
+  //printf("The size is %d \n",size);
 
+  //check if there is right amount of inputs
   if (argc != 3) {
     if (rank == 0) {
       printf("Usage: mpiexec -n <num_processes> %s <num_bins> <filename>\n", argv[0]);
@@ -24,10 +24,13 @@ int main(int argc, char *argv[]) {
     return 0;
   }
   //printf("IM rank %d \n", rank);
+
+  // get the number of bins and filename
   num_bins = atoi(argv[1]);
   filename = argv[2];
 
 
+  // If the number of bins is less than 0 or bigger its wrong input
   if (num_bins <= 0 || num_bins > 50) {
     if (rank == 0) {
       printf("Number of bins must be between 1 and 50\n");
@@ -35,7 +38,18 @@ int main(int argc, char *argv[]) {
     MPI_Finalize();
     return 0;
   }
+  // the number of process has to be between 0 and 50
+  if (size <= 0 || size > 50) {
+    if (rank == 0) {
+      printf("Number of process must be between 1 and 50\n");
+    }
+    MPI_Finalize();
+    return 0;
+  }
+
   FILE *fp =NULL;
+
+  // if the rank is 0 meaning root process open the file
   if (rank == 0){
     fp = fopen(filename, "r");
     if (!fp) {
@@ -45,11 +59,13 @@ int main(int argc, char *argv[]) {
     }
     // num_elements is a pointer and points to the fp. %d is digit that will be pointed towards
     fscanf(fp, "%d", &num_elements);
-    printf("num elements %d\n", num_elements);
+    //printf("num elements %d\n", num_elements);
   }
 
+  // collectively send the other processes the number of elements that we will render
   MPI_Bcast(&num_elements, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+  //dynamic allocation of space for the data that we have gathered from file
   int *data = (int*) malloc(num_elements * sizeof(int));
 
   // read the integers into the array
@@ -57,28 +73,30 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < num_elements; i++) {
       fscanf(fp, "%d", &data[i]);
     }
-    for (int i = 0; i < num_elements; i++) {
-      printf("data we store %d \n", data[i]);
-    }
+    // for (int i = 0; i < num_elements; i++) {
+    //   printf("data we store %d \n", data[i]);
+    // }
     fclose(fp);
   }
 
 
   // data array holds every value that we took from the file now.
-  // it was &data
+  // broadcast to all process the data we now store
   MPI_Bcast(data, num_elements, MPI_INT, 0, MPI_COMM_WORLD);
-  //num bins should it be broadcaster?
 
+
+  //number of elements that each process will crunch.
   int elements_per_proc = ceil(num_elements * 1.0 / size);
-  if(rank ==0){
-    printf("elements per data  %d \n" ,elements_per_proc);
-  }
+  // if(rank ==0){
+  //   printf("elements per data  %d \n" ,elements_per_proc);
+  // }
 
   //printf("my rank right now %d\n",rank  );
 
   // dynamically allocate space for the local histogram
   local_histogram = (int*) calloc(num_bins, sizeof(int));
 
+  //calculate the start and end index for to traverse the array
   int start_index = rank * elements_per_proc;
   int end_index = (rank + 1) * elements_per_proc;
   if (end_index > num_elements) {
@@ -86,13 +104,12 @@ int main(int argc, char *argv[]) {
   }
   //printf("as a rank %d the start is %d and end is %d \n",rank, start_index,end_index);
 
-  for (int i = 0; i < num_elements; i++) {
-    printf("later data we store %d \n", data[i]);
-  }
 
+  //traverse the data set between the start and stop index and perfor calculations
   for (int i = start_index; i < end_index; i++) {
     num = data[i];
-    printf("This is what we have for rank %d and the data %f \n",rank , num);
+
+    // for each bin check if the data fits in there.
     for (int j = 0; j < num_bins; j++) {
       range_size = 100.0 / num_bins;
       if (num >= j * range_size && num < (j + 1) * range_size) {
@@ -101,25 +118,30 @@ int main(int argc, char *argv[]) {
       }
     }
   }
-  for(int i =0; i< num_bins; i++){
-    printf("rank %d local histogram[%d] = %d\n",rank ,i,local_histogram[i] );
-  }
+  // for(int i =0; i< num_bins; i++){
+  //   printf("rank %d local histogram[%d] = %d\n",rank ,i,local_histogram[i] );
+  // }
 
   //printf("Do we still have rank? %d \n", rank );
 
+  // this is the final result dynamically allocated
   global_histogram = (int*) calloc(num_bins, sizeof(int));
 
-
+  // sum up every local histogram in the global historgram
   MPI_Reduce(local_histogram, global_histogram, num_bins, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
+  // print the global histogram
   if (rank == 0) {
     for (int k = 0; k < num_bins; k++) {
       printf("bin[%d] = %d\n", k, global_histogram[k]);
     }
     free(data);
+  }else{
+    free(data);
   }
-  printf("do we have rank here? %d\n",rank);
+  //printf("do we have rank here? %d\n",rank);
 
+  // free the dynamically allocated arrays
   free(local_histogram);
   free(global_histogram);
 
